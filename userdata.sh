f326302067bc5051e3b66e53a416dc5ee8847858
Sagar <coder-ocean@LAPTOP-OJTMFSSQ.localdomain>
2026-03-09 03:33:19 +0530

    Dockerfile corrected

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt update -y
-apt install -y git docker.io docker-compose
+apt install -y git docker.io docker-compose-plugin
 
 systemctl start docker
 systemctl enable docker
@@ -17,4 +17,4 @@ git clone https://github.com/coder-ocean/flask-express-ec2-app.git
 cd flask-express-ec2-app
 
 # Run containers
-docker-compose up -d
+docker compose up -d
